7fdeb84fff7f4794fbd3eb1f0fc4600f7f5614ab
Geraldy <geraldy.miero@gmail.com>
2020-12-29 01:35:27 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { BrowserRouter as Router } from 'react-router-dom'
+import { GlobalStyle } from "./globalStyles";
+import Hero from "./components/Hero";
+
+function App() {
+  return (
+    <Router>
+      <GlobalStyle />
+      <Hero />
+    </Router>
+  );
+}
+
+export default App;
